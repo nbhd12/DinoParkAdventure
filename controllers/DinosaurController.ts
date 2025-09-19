a82146f75eaf8babced2d57bba6a305b355a1b2e
nbhd12 <nupur124@gmail.com>
2025-09-19 16:46:50 +0200

    DinoParkFooter

--- controllers/DinosaurController.ts
+++ controllers/DinosaurController.ts
@@ -5,7 +5,7 @@ export class DinosaurController extends Controller {
     public async browsedinosaur () {
         const repo = new DinosaurRepository ();
         const dinos = await repo.findAll(); 
-        console.log ("Dinos from DB:", dinos);
+        // console.log ("Dinos from DB:", dinos);
 
         this.response.render("pages/clientDinoListing", { dinos,
 
